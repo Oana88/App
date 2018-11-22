8231f6d1a8fca75cdcc82cb47f544a19f112f422
Oana <oana.mirona@yahoo.ro>
2018-11-22 18:56:24 +0100

    4.6:added info to orders controlled, index and show pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
+  get 'simple_pages/landing_page'
+  get 'products/index'
+  get 'orders/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'simple_pages#landing_page'
+  root 'simple_pages#index'
 end
