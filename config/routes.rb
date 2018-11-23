32aeb4bd3c6013e00e7576a383526a10ed95b50c
Oana <oana.mirona@yahoo.ro>
2018-11-23 20:09:46 +0100

    5:1 modified product page to have a show product option and added a delete option to product

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
-  get 'products/index'
-  get 'orders/index'
+  get '/products/:id', to: 'products#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'simple_pages#index'
 end
