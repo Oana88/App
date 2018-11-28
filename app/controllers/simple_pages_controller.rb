d7c2fbf0d7c6ca7363e29ef9f83ea6c91dbfab2a
Oana <oana.mirona@yahoo.ro>
2018-11-28 16:51:53 +0100

    5.3:mailers

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -7,6 +7,13 @@ class SimplePagesController < ApplicationController
     @products = Product.limit(3)
   end
 
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email, to: 'oana.mirona@yahoo.ro', subject: "A new contact form message from #{@name}", body: @message).deliver_now
+  end
+
 
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get '/products/:id', to: 'products#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'simple_pages#index'
+  post 'simple_pages/thank_you'
 end
