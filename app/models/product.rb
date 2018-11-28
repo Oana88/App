18e04bbd0226cb806460a22f1f9109e2d7645cea
Oana <oana.mirona@yahoo.ro>
2018-11-28 12:20:37 +0100

    5.2: corrected search logic

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,10 @@
 class Product < ApplicationRecord
-
     def self.search(search_term)
-        Product.where("name LIKE ?", "%#{search_term}%")
-    end
+        if Rails.env.production?
+            Product.where("name ilike ?", "%#{search_term}%")
+        else
+            Product.where("name LIKE ?", "%#{search_term}%")
+            end
+        end
 
-end
+    end
