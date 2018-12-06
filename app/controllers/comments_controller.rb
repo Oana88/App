b584610e2f82473538a4d76697d3ed86faf7dd53
Oana <oana.mirona@yahoo.ro>
2018-12-06 22:28:44 +0100

    5.10:added admin roles

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,12 @@ class CommentsController < ApplicationController
         @product = Product.find(params[:product_id])
         @comment = @product.comments.new(comment_params)
         @comment.user = current_user
+        @user = current_user
 
         respond_to do |format|
             if @comment.save
                 format.html {redirect_to @product, notice: 'Review was created successfully.'}
-                format.json {render :show, status: :created, location: @product}
+                format.json {render :show, status: :created, location: @product} 
             else
                 format.html {redirect_to @product, alert: 'Review was not saved successfully'}
                 format.json {render json: @comment.errors, status: :unprocessable_entity}
@@ -17,10 +18,13 @@ class CommentsController < ApplicationController
     end
 
     def destroy
+        @comment = Comment.find(params[:id])
+        product = @comment.product
+        @comment.destroy
+        redirect_to product
     end
 
     private
-
     def comment_params
         params.require(:comment).permit(:user_id, :body, :rating)
     end
